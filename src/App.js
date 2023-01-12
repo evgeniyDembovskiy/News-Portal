b755a5403022cfceed0161abd668fb8cdd23aceb
Evgeniy <dembovski4@mail.ru>
2023-01-12 18:27:19 +0300

    post list and adding posts are done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles/App.css"
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
+
 
 function App() {
-  return (
-    <div className="App">
+    const [posts, setPosts] = useState([
+        {id: 1, title: "JavaScript 1", body: "JavaScript - язык программирования"},
+        {id: 2, title: "JavaScript 2", body: "JavaScript - язык программирования"},
+        {id: 3, title: "JavaScript 3", body: "JavaScript - язык программирования"},
+    ]);
 
-    </div>
-  );
+    const [post, setPost] = useState({title: "", body: "",});
+
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        setPosts([...posts, {...post, id: Date.now()}]);
+        setPost({title: "", body: ""});
+    }
+
+
+    return (
+        <div className="App">
+            <form>
+                <MyInput 
+                    onChange={e => setPost({...post, title: e.target.value})} 
+                    value={post.title} 
+                    type="text" 
+                    placeholder="Название поста"/>
+                <MyInput 
+                    onChange={e => setPost({...post, body: e.target.value})} 
+                    value={post.body} 
+                    type="text" 
+                    placeholder="Описание поста"
+                />
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title="Список новостей"/>
+        </div>
+    );
 }
 
 export default App;
